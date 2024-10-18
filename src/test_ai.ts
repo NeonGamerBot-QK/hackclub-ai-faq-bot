084d99ef57c089508f7e732c20308338082ca833
Saahil <neon@saahild.com>
2024-10-18 17:53:03 +0000

    feat(tests): test ai

---
+++ src/test_ai.ts
@@ -0,0 +1,33 @@
+// help idk how to use assistns
+import openai from "openai";
+const ai = new openai({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+const aiID = await ai.beta.assistants.list().then((e) => e.data[0].id);
+const thread = await ai.beta.threads.create();
+const message = await ai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content: "What is hackclub?"
+    }
+);
+  
+let run = await ai.beta.threads.runs.createAndPoll(
+    thread.id,
+    { 
+      assistant_id: aiID,
+    //   instructions: "Please address the user as Jane Doe. The user has a premium account."
+    }
+);
+if (run.status === 'completed') {
+    const messages = await ai.beta.threads.messages.list(
+      run.thread_id
+    );
+    for (const message of messages.data.reverse()) {
+        //@ts-ignore
+      console.log(`${message.role} > ${message.content[0].text.value}`);
+    }
+  } else {
+    console.log(run.status);
+  }
